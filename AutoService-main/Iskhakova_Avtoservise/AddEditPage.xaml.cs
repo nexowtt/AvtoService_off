4dc0ebacacd1dfb14040dcb98a6c5e67f4d36484
nexowtt <iskhakovazalina99@gmail.com>
2024-10-27 01:45:40 +0300

    7lb

---
+++ AutoService-main/Iskhakova_Avtoservise/AddEditPage.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Iskhakova_Avtoservise
+{
+    /// <summary>
+    /// Логика взаимодействия для AddEditPage.xaml
+    /// </summary>
+    public partial class AddEditPage : Page
+    {
+        private Service _currentServise = new Service();
+        public AddEditPage(Service SelectedService)
+        {
+            InitializeComponent();
+            if (SelectedService != null)
+            {
+                _currentServise = SelectedService;
+            }
+            DataContext = _currentServise;
+        }
+
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder errors = new StringBuilder();
+            if (string.IsNullOrWhiteSpace(_currentServise.Title))
+                errors.AppendLine("Укажите название услуги");
+            if (_currentServise.Cost == 0)
+                errors.AppendLine("Укажите стоимость услуги");
+            if (_currentServise.Discount == null)
+                errors.AppendLine("Укажите скидку");
+            if (_currentServise.Discount < 0)
+                errors.AppendLine("Укажите скидку");
+            if (_currentServise.Discount > 1)
+                errors.AppendLine("Укажите скидку");
+            if (string.IsNullOrWhiteSpace(_currentServise.Duration))
+                errors.AppendLine("Укажите длительность услуги");
+
+            if (errors.Length > 0)
+            {
+                MessageBox.Show(errors.ToString());
+                return;
+            }
+            if (_currentServise.ID == 0)
+                iskhakova_avtoserviceEntities.GetContext().Service.Add(_currentServise);
+            try
+            {
+                iskhakova_avtoserviceEntities.GetContext().SaveChanges();
+                MessageBox.Show("информация сохранена");
+                Manager.MainFrame.GoBack();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+    }
+}
---
+++ AutoService-main/Iskhakova_Avtoservise/ServicePage.xaml.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Iskhakova_Avtoservise
+{
+    /// <summary>
+    /// Логика взаимодействия для ServicePage.xaml
+    /// </summary>
+    public partial class ServicePage : Page
+    {
+        public ServicePage()
+        {
+            InitializeComponent();
+            //добавляем строки
+            //загрузить в список из бд
+            var currentServies = iskhakova_avtoserviceEntities.GetContext().Service.ToList();
+            //связать с нашим листвью
+            ServiceListView.ItemsSource=currentServies;
+            //добавили строки
+
+            ComboType.SelectedIndex=0;
+            UpdateServices();
+        }
+        private void TBoxSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateServices();
+        }
+
+        private void ComboType_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateServices();
+        }
+
+        private void RadioButton_Checked_1(object sender, RoutedEventArgs e)
+        {
+            UpdateServices();
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            UpdateServices();
+        }
+
+        private void UpdateServices()
+        {
+            var currentServices = iskhakova_avtoserviceEntities.GetContext().Service.ToList();
+
+            if (ComboType.SelectedIndex == 0)
+            {
+                currentServices = currentServices.Where(p => (p.DiscountInt >= 0 && p.DiscountInt <= 100)).ToList();
+            }
+            if (ComboType.SelectedIndex == 1)
+            {
+                currentServices = currentServices.Where(p => (p.DiscountInt >= 0 && p.DiscountInt < 5)).ToList();
+            }
+            if (ComboType.SelectedIndex == 2)
+            {
+                currentServices = currentServices.Where(p => (p.DiscountInt >= 5 && p.DiscountInt < 15)).ToList();
+            }
+            if (ComboType.SelectedIndex == 3)
+            {
+                currentServices = currentServices.Where(p => (p.DiscountInt >= 15 && p.DiscountInt < 30)).ToList();
+            }
+            if (ComboType.SelectedIndex == 4)
+            {
+                currentServices = currentServices.Where(p => (p.DiscountInt >= 30 && p.DiscountInt < 70)).ToList();
+            }
+            if (ComboType.SelectedIndex == 5)
+            {
+                currentServices = currentServices.Where(p => (p.DiscountInt >= 70 && p.DiscountInt < 100)).ToList();
+            }
+        ///fsdfdsfdsfdsfsd
+
+            currentServices = currentServices.Where(p => p.Title.ToLower().Contains(TBoxSearch.Text.ToLower())).ToList();
+            ServiceListView.ItemsSource = currentServices.ToList();
+
+            if (RButtonDown.IsChecked.Value)
+            {
+                ServiceListView.ItemsSource=currentServices.OrderByDescending(p => p.Cost).ToList();
+            }
+
+            if (RButtonUP.IsChecked.Value)
+            {
+                ServiceListView.ItemsSource = currentServices.OrderBy(p => p.Cost).ToList();
+            }
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            //открыть окно добавления услуг или редактирования 
+            Manager.MainFrame.Navigate(new AddEditPage(null));
+        }
+
+        private void EditButton_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage((sender as Button).DataContext as Service));
+        }
+        private void Page_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (Visibility == Visibility.Visible)
+            {
+                iskhakova_avtoserviceEntities.GetContext().ChangeTracker.Entries().ToList().ForEach(p => p.Reload());
+                ServiceListView.ItemsSource = iskhakova_avtoserviceEntities.GetContext().Service.ToList();
+            }
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            //забираем Сервис, для которого нажата кнопка "Удалить"
+            var currentService = (sender as Button).DataContext as Service;
+            //проверка на возможность удаления
+            var currentClientServices = iskhakova_avtoserviceEntities.GetContext().ClientService.ToList();
+            currentClientServices=currentClientServices.Where(p => p.ServiceID == currentService.ID).ToList();
+
+            if (currentClientServices.Count != 0) //усли есть записи на этот сервис
+                MessageBox.Show("Невозможно выполнить удаление, т.к. существуют записи на эту услугу");
+            else
+            {
+                if (MessageBox.Show("Вы точно хотите выполнить удаление?", "Внимание!",
+                    MessageBoxButton.YesNo, MessageBoxImage.Question)==MessageBoxResult.Yes)
+                {
+                    try
+                    {
+                        iskhakova_avtoserviceEntities.GetContext().Service.Remove(currentService);
+                        iskhakova_avtoserviceEntities.GetContext().SaveChanges();
+                        //выводим в листвью измененую таблицу Сервис
+                        ServiceListView.ItemsSource=iskhakova_avtoserviceEntities.GetContext().Service.ToList();
+                        //чтобы применились фильтры и поиск, если они были на форме изначально
+                        UpdateServices();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message.ToString());
+                    }
+                }
+            }
+        }
+    }
+}
