12be2c963051450f99c8e5a74852259ac95546f4
nexowtt <iskhakovazalina99@gmail.com>
2024-10-30 13:11:07 +0300

    7.2lb

--- AutoService-main/Iskhakova_Avtoservise/ServicePage.xaml.cs
+++ AutoService-main/Iskhakova_Avtoservise/ServicePage.xaml.cs
@@ -20,6 +20,11 @@ namespace Iskhakova_Avtoservise
     /// </summary>
     public partial class ServicePage : Page
     {
+        int CountRecords;//Количество записей в таблице
+        int CountPage; //Общее количество страниц
+        int CurrentPage;//Текущая страница
+        List<Service> CurrentPageList = new List<Service>();
+        List<Service> TableList;
         public ServicePage()
         {
             InitializeComponent();
@@ -81,20 +86,30 @@ namespace Iskhakova_Avtoservise
             {
                 currentServices = currentServices.Where(p => (p.DiscountInt >= 70 && p.DiscountInt < 100)).ToList();
             }
-        ///fsdfdsfdsfdsfsd
+            ///fsdfdsfdsfdsfsd
 
             currentServices = currentServices.Where(p => p.Title.ToLower().Contains(TBoxSearch.Text.ToLower())).ToList();
             ServiceListView.ItemsSource = currentServices.ToList();
 
             if (RButtonDown.IsChecked.Value)
             {
-                ServiceListView.ItemsSource=currentServices.OrderByDescending(p => p.Cost).ToList();
+                //для отображения итогов фильтра и поиска в листвью по убыванию
+                currentServices=currentServices.OrderByDescending(p => p.Cost).ToList();
             }
 
             if (RButtonUP.IsChecked.Value)
             {
-                ServiceListView.ItemsSource = currentServices.OrderBy(p => p.Cost).ToList();
+                //для отображения итогов фильтра и поиска в листвью по возрастанию
+                currentServices = currentServices.OrderBy(p => p.Cost).ToList();
             }
+            // для отображения итогов фильтра и поиска в листвью
+            ServiceListView.ItemsSource=currentServices;
+            //заполнение таблицы для постраничного вывоода
+            TableList=currentServices;
+            //вызов функции отображения кол-во страниц с параметрами:
+            //направленрия 0 - начальная загрузка
+            //0 - выбранная страница
+            ChangePage(0, 0);
         }
 
         private void AddButton_Click(object sender, RoutedEventArgs e)
@@ -113,6 +128,7 @@ namespace Iskhakova_Avtoservise
             {
                 iskhakova_avtoserviceEntities.GetContext().ChangeTracker.Entries().ToList().ForEach(p => p.Reload());
                 ServiceListView.ItemsSource = iskhakova_avtoserviceEntities.GetContext().Service.ToList();
+                UpdateServices();
             }
         }
 
@@ -146,6 +162,121 @@ namespace Iskhakova_Avtoservise
                     }
                 }
             }
+        }
+
+        private void LeftDirButton_Click(object sender, RoutedEventArgs e)
+        {
+            ChangePage(1, null);
+        }
+
+        private void RightDirButton_Click(object sender, RoutedEventArgs e)
+        {
+            ChangePage(2, null);
+        }
+
+        private void ChangePage(int direction, int? selectedPage)//Функция отвечающая за разделение list'a
+        {
+            //direction - направление. 0 - начало, 1- предыдущая страница, 2 следующая страница
+            //selectedPage - при нажатии на стрелочки передается null,
+            // при выборе определенной страницы в этой переменной находится номер страницы
+            CurrentPageList.Clear();// начальная очистка листа
+            CountRecords = TableList.Count;//определение количества записей во всем списке
+            // определение кол-ва страниц
+            if (CountRecords % 10 > 0)
+            {
+                CountPage = CountRecords / 10 + 1;
+            }
+            else
+            {
+                CountPage = CountRecords / 10;
+            }
+            Boolean Ifupdate = true;
+            // Проверка на правильность-если
+            //CurrentPage(номер текущей страница) "правильный"
+
+            int min;
+
+            if (selectedPage.HasValue)//Проверка на значение не пull (TK может быть null)
+            {
+                if (selectedPage >= 0 && selectedPage <= CountPage)
+                {
+                    CurrentPage = (int)selectedPage;
+                    min = CurrentPage * 10 + 10 < CountRecords ? CurrentPage * 10 + 10 : CountRecords;
+                    for (int i = CurrentPage * 10; i < min; i++)
+                    {
+                        CurrentPageList.Add(TableList[i]);
+                    }
+                }
+            }
+            else // если нажата стрелка
+            {
+                switch (direction)
+                {
+                    case 1://нажата кнопка "Предыдущая страница"
+                        if (CurrentPage > 0)
+                        // то есть кнопка нажата правильно и "назад" можно идти
+                        {
+                            CurrentPage--;
+                            min = CurrentPage * 10 + 10 < CountRecords ? CurrentPage *10 + 10 : CountRecords;
+                            for (int i = CurrentPage * 10; i < min; i++)
+                            {
+
+                                CurrentPageList.Add(TableList[i]);
+                            }
+                        }
+                        else
+                        {
+                            Ifupdate = false;
+                            //в случае если currentPage попытаутся выйти из диапазона внесения данных не произойдет
+                        }
+                        break;
+
+                    case 2://нажата кнопка "следующая страница"
+                        if (CurrentPage < CountPage - 1)
+                        //если вперед идти можно
+                        {
+                            CurrentPage++;
+                            min=CurrentPage * 10 + 10 < CountRecords ? CurrentPage * 10 + 10 : CountRecords;
+                            for (int i = CurrentPage * 10; i<min; i++)
+                            {
+                                CurrentPageList.Add(TableList[i]);
+                            }
+                        }
+                        else
+                        {
+                            Ifupdate=false;
+                        }
+                        break;
+                }
+            }
+            
+                if (Ifupdate)//если currentPage не вышел из диапазона, то
+                {
+                    PageListBox.Items.Clear();
+                    // Удаление старых значений из listBox'а номеров страниц, нужно чтобы при изменении
+                    // кол-ва записей кол-во страниц динамически изменялось
+                    for (int i = 1; i <= CountPage; i++)
+                    {
+                        PageListBox.Items.Add(i);
+                    }
+                    PageListBox.SelectedIndex = CurrentPage;
+
+                    //вывод коли-ва записей на странице и общего кол-ва
+                    min = CurrentPage * 10 +10 < CountRecords ? CurrentPage * 10 + 10 : CountRecords;
+                    TBCount.Text = min.ToString();
+                    TBAllRecords.Text = " из " + CountRecords.ToString();
+
+
+                    ServiceListView.ItemsSource = CurrentPageList;
+                    //обновить отображение списка услуг
+                    ServiceListView.Items.Refresh();
+                }
+            }
+        
+
+        private void PageListBox_MouseUp_1(object sender, MouseButtonEventArgs e)
+        {
+            ChangePage(0, Convert.ToInt32(PageListBox.SelectedItem.ToString()) - 1);
         }
     }
 }
